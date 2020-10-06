0c0d2cae824bf11210fa37db9578b9c3bd9e32ab
bfin00 <71728839+bfin00@users.noreply.github.com>
2020-10-06 13:06:22 +0300

    Create model.cpp

---
+++ model.cpp
@@ -0,0 +1,13 @@
+#include "Model.h"
+#include <QGroupBox>
+#include <QGridLayout>
+#include <QLabel>
+#include <QTableWidgetItem>
+Model::Model(QWidget *parent): QWidget(parent)
+{
+
+}
+void Model::setUpWidgets()
+{
+
+}
