91e584b979a94732e4ec87d2963883cef8bb9e31
bfin00 <71728839+bfin00@users.noreply.github.com>
2020-10-07 20:35:07 +0300

    Update widgets.cpp

--- widgets.cpp
+++ widgets.cpp
@@ -16,19 +16,26 @@ void Widgets::setupWidgets()
     box1 = new QGroupBox(tr("Введите данные здесь"), this);
     QHBoxLayout *top_l = new QHBoxLayout(box1);
     QPushButton *but = new QPushButton(tr("Сохранить"));
-    QLabel *lbl1 = new QLabel(tr("Количество стержней"));
+    QLabel *lbl1 = new QLabel(tr("Количество\nстержней"));
     line1 = new QLineEdit;
-    QLabel *lbl2 = new QLabel(tr("Количество типов стержней"));
+    QLabel *lbl2 = new QLabel(tr("Количество\nтипов стержней"));
     line2 = new QLineEdit;
-    QLabel *lbl3 = new QLabel(tr("Количество узлов"));
+    QLabel *lbl3 = new QLabel(tr("Количество\nузлов"));
     line3 = new QLineEdit;
-
+    QLabel *lbl4 = new QLabel(tr("Сосредоточенные\nнагрузки"));
+    line4 = new QLineEdit;
+    QLabel *lbl5 = new QLabel(tr("Продальные\nнагрузки"));
+    line5 = new QLineEdit;
     top_l->addWidget(lbl1);
     top_l->addWidget(line1);
     top_l->addWidget(lbl2);
     top_l->addWidget(line2);
     top_l->addWidget(lbl3);
     top_l->addWidget(line3);
+    top_l->addWidget(lbl4);
+    top_l->addWidget(line4);
+    top_l->addWidget(lbl5);
+    top_l->addWidget(line5);
     top_l->addWidget(but);
 
     box2 = new QGroupBox(this);
@@ -36,7 +43,7 @@ void Widgets::setupWidgets()
     t_1 = new QTableWidget(1, 1, this);
     t_2 = new QTableWidget(1, 1, this);
     t_3 = new QTableWidget(1, 4, this);
-    t_4 = new QTableWidget(1, 1, this);
+    t_4 = new QTableWidget(2, 1, this);
     t_5 = new QTableWidget(1, 2, this);
     t_6 = new QTableWidget(1, 2, this);
 
@@ -46,13 +53,27 @@ void Widgets::setupWidgets()
     t_4->setMaximumSize(160, 200);
     t_5->setMaximumSize(160, 200);
     t_6->setMaximumSize(160, 200);
-
     bot_l->addWidget(t_1);
     bot_l->addWidget(t_2);
     bot_l->addWidget(t_3);
     bot_l->addWidget(t_4);
     bot_l->addWidget(t_5);
     bot_l->addWidget(t_6);
-    
-    connect(line1, SIGNAL(textChanged(const QString)), this, SLOT(setTab(int)));
+
+    connect(but, &QPushButton::clicked, this, &Widgets::setUpTables);
+    connect(t_1, &QTableWidget::cellChanged, this, &Widgets::addValuesToV);
 }
+
+void Widgets::setUpTables()
+{
+    t_2->setRowCount(line1->text().toInt());
+    t_3->setRowCount(line2->text().toInt());
+    t_1->setRowCount(line3->text().toInt());
+    t_5->setRowCount(line4->text().toInt());
+    t_6->setRowCount(line5->text().toInt());
+}
+void Widgets::addValuesToV(int r, int c)
+{
+
+}
+
