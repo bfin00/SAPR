0acbbca8a3705406c5311930a350b651596919bc
bfin00 <71728839+bfin00@users.noreply.github.com>
2020-10-26 15:08:13 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,33 @@
 #include "mainwindow.h"
 #include "Tables.h"
 
+#include <QtWidgets>
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
 {
+
     _tables = new Tables{ this };
+    setUpMenus();
     connect(_tables, &Tables::passParams, this, &MainWindow::calcResult);
     setWindowTitle(QStringLiteral("САПР"));
     setCentralWidget(_tables);
     showMaximized();
 }
 
+void MainWindow::setUpMenus()
+{
+    auto fileMenu = menuBar()->addMenu(tr("File"));
+
+    auto *saveAct = new QAction(tr("Save") ,this);
+    fileMenu->addAction(saveAct);
+    connect(saveAct, &QAction::triggered, _tables, &Tables::save);
+    auto *openAct = new QAction(tr("Open"), this);
+    fileMenu->addAction(openAct);
+    connect(openAct, &QAction::triggered, _tables, &Tables::read);
+    auto exitAction = fileMenu->addAction(tr("E&xit"), qApp, &QCoreApplication::quit);
+    exitAction->setShortcuts(QKeySequence::Quit);
+
+}
 MainWindow::~MainWindow() = default;
 
 void MainWindow::calcResult(const Params& params)
