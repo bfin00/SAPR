6f8a7d063a83f1fa5dc1b4c24d17214168360005
bfin00 <71728839+bfin00@users.noreply.github.com>
2020-12-02 12:45:19 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,37 +1,58 @@
 #include "mainwindow.h"
 #include "Tables.h"
+#include "RenderArea.h"
 
 #include <QtWidgets>
+#include "resultwindow.h"
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
 {
 
+    box = new QGroupBox(this);
+    mainLayout = new QVBoxLayout;
     _tables = new Tables{ this };
+    _area = new RenderArea();
+
+    stackedWidget = new QStackedWidget(this);
+
+    mainLayout->addWidget(_tables);
+    mainLayout->addWidget(_area);
+    box->setLayout(mainLayout);
     setUpMenus();
-    connect(_tables, &Tables::passParams, this, &MainWindow::calcResult);
+
     setWindowTitle(QStringLiteral("САПР"));
-    setCentralWidget(_tables);
+    auto _resultWindow = new ResultWindow(this);
+    _resultWindow->setMinimumSize(400, 400);
+    stackedWidget->addWidget(box);
+    stackedWidget->addWidget(_resultWindow);
+    stackedWidget->setCurrentIndex(0);
+    setCentralWidget(stackedWidget);
     showMaximized();
+    connect(_tables, &Tables::passParamsForDrawing, _area, &RenderArea::drawConstruction);
+    connect(_tables, &Tables::passParams, _resultWindow, &ResultWindow::calcResuluts);
+    connect(_tables, &Tables::passParams, this, &MainWindow::setResultWindow);
+    connect(_resultWindow, &ResultWindow::returnBack, this, &MainWindow::setTableWindow);
 }
 
 void MainWindow::setUpMenus()
 {
     auto fileMenu = menuBar()->addMenu(tr("File"));
 
-    auto *saveAct = new QAction(tr("Save") ,this);
+    auto *saveAct = new QAction(tr("&Save") ,this);
     fileMenu->addAction(saveAct);
     connect(saveAct, &QAction::triggered, _tables, &Tables::save);
-    auto *openAct = new QAction(tr("Open"), this);
+    auto *openAct = new QAction(tr("&Open"), this);
     fileMenu->addAction(openAct);
     connect(openAct, &QAction::triggered, _tables, &Tables::read);
     auto exitAction = fileMenu->addAction(tr("E&xit"), qApp, &QCoreApplication::quit);
     exitAction->setShortcuts(QKeySequence::Quit);
 
 }
+
+void MainWindow::setTableWindow() {stackedWidget->setCurrentIndex(0);}
+
+void MainWindow::setResultWindow(){stackedWidget->setCurrentIndex(1);}
+
 MainWindow::~MainWindow() = default;
 
-void MainWindow::calcResult(const Params& params)
-{
-//обработка результатов(возможно, вынести в отдельный класс)
-}
 
