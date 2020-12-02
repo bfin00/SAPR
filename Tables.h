99760ea50abeb0219d8956fcda2f977d78cbf701
bfin00 <71728839+bfin00@users.noreply.github.com>
2020-12-02 12:46:05 +0300

    Update Tables.h

--- Tables.h
+++ Tables.h
@@ -6,6 +6,7 @@ class QLineEdit;
 class QGroupBox;
 class QTableWidget;
 class QPushButton;
+class QCheckBox;
 
 struct Params
 {
@@ -14,6 +15,8 @@ struct Params
     QVector<int> centred_loads;
     QVector<int> distr_loads;
     QVector<QVector<double>> types_descrip;
+
+    QVector<QVector<double>> type_for_each_rod;
 };
 
 class Tables : public QWidget
@@ -27,16 +30,22 @@ public:
 
 signals:
     void passParams(const Params& params);
+    void passParamsForDrawing(const Params& params);
 
 public slots:
     bool save();
     void read();
+    void saveParams();
 
 private slots:
     void setParamTables();
     void prepareParams();
+    void sendParamsForDraw();
 
 private:
+    int tempp;
+
+    QCheckBox *left, *right;
     QTableWidget  * _t_2, * _t_3, * _t_4, * _t_5, * _t_6;
     QLineEdit* _line1, * _line2, * _line3;
     QGroupBox* _box1;
@@ -53,6 +62,7 @@ private:
     void addValuesToV(QTableWidget* table, QVector<QVector<T>>& vector);
     template <typename T>
     void addValuesToV(QTableWidget* table, QVector<T>& vector);
+    void convertLoads(QVector<int>& vec);
 
     void setHeaders();
     void setWidgetSize();
@@ -60,17 +70,22 @@ private:
     bool saveFile(const QString& fileName);
     bool checkFixedNodes();
     bool checkRods();
+
     void readFromFile(const QString& fileName);
     void readIntoVecs(QTextStream& in, QVector<int>& vec);
     void readIntoVecs(QTextStream& in, QVector<QVector<double>>& vec);
     void setTables();
     void setTable(const QVector<int>& vec, QTableWidget* table);
     void setTable(const QVector<QVector<double>>& vec, QTableWidget* table);
+    void checkBoxForLeft();
+    void checkBoxForRight();
+    void checkEmptyCells(const QTableWidget* t);
 
-    template <typename T>
-    void writeToFile(QTextStream& out, const QVector<QVector<T>>& vec);
+
+    void writeToFile(QTextStream& out, const QVector<QVector<double>>& vec);
     template <typename T>
     void writeToFile(QTextStream& out, const QVector<T>& vec);
 
     void drawConstruction();
 };
+
